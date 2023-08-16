710e27725aa8e8c46e06d98a2276d92d94ba8fc7
GUNCEL\omer.bulbul <omer.bulbul@guncelyazilim.com.tr>
2023-08-16 09:54:59 +0300

    Yeni tablolar eklendi.

---
+++ Tables/dbo.BookLoans.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[BookLoans] (
+  [BookLoanID] [int] IDENTITY,
+  [BorrewerID] [int] NULL,
+  [LibraryID] [int] NULL,
+  [DateOut] [smalldatetime] NULL,
+  [DueDate] [smalldatetime] NULL,
+  CONSTRAINT [PK_BookLoans_BookLoanID] PRIMARY KEY CLUSTERED ([BookLoanID])
+)
+ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[BookLoans]
+  ADD CONSTRAINT [FK_BookLoans_Borrewers_BorrewerID] FOREIGN KEY ([BorrewerID]) REFERENCES [dbo].[Borrewers] ([BorrewerID])
+GO
+
+ALTER TABLE [dbo].[BookLoans]
+  ADD CONSTRAINT [FK_BookLoans_Libraries_LibraryID] FOREIGN KEY ([LibraryID]) REFERENCES [dbo].[Libraries] ([LibraryID])
+GO
---
+++ Tables/dbo.Borrewers.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Borrewers] (
+  [BorrewerID] [int] IDENTITY,
+  [BorrewerCode] [varchar](50) NULL,
+  [BorrewerName] [varchar](50) NULL,
+  [BorrewerCardNo] [varchar](50) NULL,
+  CONSTRAINT [PK_Borrewers_BorrewerID] PRIMARY KEY CLUSTERED ([BorrewerID])
+)
+ON [PRIMARY]
+GO
